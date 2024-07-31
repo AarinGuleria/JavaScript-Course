7bd2d2a46cc6460bfcfb8642f4ab2ca5989f3a84
AarinGuleria <aaringuleria2310@gmail.com>
2024-07-31 11:55:19 +0530

    commit

---
+++ conditionalStatements/switch-case.js
@@ -0,0 +1,16 @@
+let job = "Sales Engineer";
+
+switch (job) {
+  case "Sales Engineer":
+    console.log("You are a Sales Engineer");
+    break;
+  case "Software Engineer":
+    console.log("You are a Software Engineer");
+    break;
+  case "Project Manager":
+    console.log("You are a Project Manager");
+    break;
+  default:
+    console.log("You are a Sales Executive");
+    break;
+}
---
+++ looping/for_Loop.js
@@ -0,0 +1,11 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// let idx = 0;
+// let lengthOfArray = numbers.length;
+
+// console.log(numbers[idx]); //1
+
+
+for(let idx = 0; idx < numbers.length; idx++){
+    console.log(numbers[idx]);
+}
